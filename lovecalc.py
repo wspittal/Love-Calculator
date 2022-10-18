6bb17ba6ca0c6fd68efd1ea6b941ea052aa72b3c
wspittal <114256362+wspittal@users.noreply.github.com>
2022-10-18 09:53:33 -0400

    Update lovecalc.py

--- lovecalc.py
+++ lovecalc.py
@@ -3,14 +3,11 @@ m = ""
 while m != "e":
     name1 = input("First name: ")
     name2 = input("Second name: ")
-    if name1.lower() == ("walker" or "walker spittal") or ("avery" or "avery lassiter") and name2.lower() == ("walker" or "walker spittal") or ("avery" or "avery lassiter") and name1.lower() != name2.lower():
-      print(name1 + " and " + name2 + "'s love percentage is ∞ %!")
-    else:
-      nameConglomerate = [char for char in (name1 + name2).lower()]
-      points = 1
-      totalPoints = 78
-      for i in [char for char in "abcdefghijklmnopqrstuvwxyz-0123456789 "]:
-        totalPoints += nameConglomerate.count(i) * points
-        points += 1
-      print(name1 + " and " + name2 + "'s love percentage is " + str(totalPoints % 101) + "%!")
+    nameConglomerate = [char for char in (name1 + name2).lower()]
+    points = 1
+    totalPoints = 78
+    for i in [char for char in "abcdefghijklmnopqrstuvwxyz-0123456789 "]:
+      totalPoints += nameConglomerate.count(i) * points
+      points += 1
+    print(name1 + " and " + name2 + "'s love percentage is " + str(totalPoints % 101) + "%!")
     m = input("Press enter to go again, or type \"e\" to end!")
